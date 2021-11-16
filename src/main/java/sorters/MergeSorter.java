2ff9cd1f2e97c02490926fd855eee1d3ce49f5a3
katseliv <katseliv@yandex.ru>
2021-11-16 18:25:47 +0300

    Added sorters

---
+++ src/main/java/sorters/MergeSorter.java
@@ -0,0 +1,50 @@
+package sorters;
+
+import entities.Contract;
+
+import java.util.Comparator;
+
+public class MergeSorter implements Sorter {
+
+    @Override
+    public void sort(Contract[] contracts, Comparator<Contract> comparator) {
+
+    }
+
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        System.arraycopy(a, 0, l, 0, mid);
+        if (n - mid >= 0) System.arraycopy(a, mid, r, 0, n - mid);
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            }
+            else {
+                a[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+}
